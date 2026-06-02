769486de2c499acb2f4baad1b82c3e0aab8f96d3
BBB_12BB <pongsada.brite@gmail.com>
2026-06-02 15:38:53 +0700

    chore: remove chrome cache and junk files, update gitignore

--- test_net.js
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-http.get('http://localhost:3000', (res) => {
-  console.log('STATUS:', res.statusCode);
-}).on('error', (err) => {
-  console.error('ERROR:', err);
-});
